164bb66102196847bf1ef34d733fed7a9ad83278
Renan Camargo <renan.souza@hubfintech.com.br>
2022-07-29 21:19:41 -0300

    feat: adicionando states

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,50 +1,83 @@
 import styles from './Post.module.css'
 import { Comment } from '../Comments'
 import { Avatar } from '../Avatar'
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { useState } from 'react'
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+
+  const [ comments, setComments ] = useState([
+    'Post muito bacana, ein?'
+  ])
+
+  const [ newComment, setNewComment ] = useState('')
+
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
+  function handleCreateNewComment() {
+    event.preventDefault()
+    setComments([...comments, newComment])
+    setNewComment('')
+  }
+
+  function handleNewCommentChange(){
+    setNewComment(event.target.value)
+  }
+
   return(
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src='https://github.com/rcmsouza.png' />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Renan Camargo</strong>
-            <span>Web developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
         <time
-         title={new Date().toLocaleDateString('pt-BR')} 
-         dateTime={new Date()}
+         title={publishedDateFormatted} 
+         dateTime={publishedAt.toISOString()}
         >
-          Publicado há 1h
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-          <p>Fala galeraa 👋</p>
-          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-          <p><a href="">jane.design/doctorcare</a></p>
-          <p>
-            <a href="">#novoprojeto</a>{' '}
-            <a href="">#nlw</a>{' '}
-            <a href="">#rocketseat</a>
-          </p>
+          {content.map(line => {
+            if(line.type === 'paragraph'){
+              return <p>{line.content}</p>
+            } else if(line.type === 'link') {
+              return <p><a href="#">{line.content}</a></p>
+            }
+          })}
       </div>
       
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder='Deixe um comentário' />
+        <textarea
+         onChange={handleNewCommentChange} 
+         name='comment' 
+         value={newComment}
+         placeholder='Deixe um comentário' 
+        />
         <footer>
           <button type='submit'>Publicar</button>
         </footer>
       </form>
 
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(comment => {
+          return <Comment content={comment}/>
+        })}
       </div>
 
     </article>
